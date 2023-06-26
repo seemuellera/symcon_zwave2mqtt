760788bb1d28ad495e2ed5d2e6a407e710ca7d7a
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-06-26 11:03:57 +0200

    dsf

--- Device/module.php
+++ Device/module.php
@@ -18,7 +18,7 @@ class Zwave2MQTTDevice extends IPSModule
         //Never delete this line!
         parent::Create();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
-        $this->RegisterPropertyString('MQTTBaseTopic', 'zigbee2mqtt');
+        $this->RegisterPropertyString('MQTTBaseTopic', 'zwave2mqtt');
         $this->RegisterPropertyString('MQTTTopic', '');
         $this->createVariableProfiles();
     }
@@ -29,13 +29,11 @@ class Zwave2MQTTDevice extends IPSModule
         parent::ApplyChanges();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
         //Setze Filter für ReceiveData
-        $Filter1 = preg_quote('"Topic":"' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '"');
-        $Filter2 = preg_quote('"Topic":"symcon/' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/');
+        $Filter = preg_quote('"Topic":"' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '"');
         //$this->SendDebug('Filter ::', $MQTTTopic, 0);
         //$this->SetReceiveDataFilter('.*' . $MQTTTopic . '.*');
 
-        $this->SendDebug('Filter ', '.*(' . $Filter1 . '|' . $Filter2 . ').*', 0);
-        $this->SetReceiveDataFilter('.*(' . $Filter1 . '|' . $Filter2 . ').*');
+        $this->SetReceiveDataFilter('.*' . $Filter . '.*');
         if (($this->HasActiveParent()) && (IPS_GetKernelRunlevel() == KR_READY)) {
             //$this->getDeviceInfo();
         }
