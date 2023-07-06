3896d11515febeb5d804cfff23fb9f5ff89947a3
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-07-06 22:48:17 +0200

    sdf

--- Configurator/module.php
+++ Configurator/module.php
@@ -13,19 +13,12 @@ class Zwave2MQTTConfigurator extends IPSModule
         parent::Create();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
         $this->RegisterPropertyString('MQTTBaseTopic', 'zwave');
-        $this->RegisterAttributeString('Devices','{]');
-
-        $this->SetBuffer('Devices', '{}');
     }
 
     public function ApplyChanges()
     {
         //Never delete this line!
         parent::ApplyChanges();
-
-        //Setze Filter für ReceiveData
-        $topic = $this->ReadPropertyString('MQTTBaseTopic');
-        $this->SetReceiveDataFilter('.*' . $topic . '.*');
         $this->SetStatus(102);
     }
 
@@ -34,8 +27,8 @@ class Zwave2MQTTConfigurator extends IPSModule
         $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
 
         //Devices
-        $Devices = json_decode($this->ReadAttributeString('Devices'), true);
-        $this->SendDebug('Buffer Devices', json_encode($Devices), 0);
+        $Devices = $this->getDeviceInfo();
+        $this->SendDebug('Devices', json_encode($Devices), 0);
         $ValuesDevices = [];
 
         foreach ($Devices as $device) {
@@ -79,100 +72,6 @@ class Zwave2MQTTConfigurator extends IPSModule
             }
         }
         return 0;
-    }
-
-    public function ReceiveData($JSONString)
-    {
-        $Buffer = json_decode($JSONString, true);
-        if (array_key_exists('Topic', $Buffer)) {
-
-            if ($Buffer ['Topic'] == $this->ReadPropertyString('MQTTBaseTopic') . '/_CLIENTS/ZWAVE-GATEWAY-zwave-js-ui/api/getNodes') {
-                $this->SendDebug('TOPIC', $Buffer['Topic'], 0);
-            }
-            else {
-
-                return;
-            }
-
-            if(array_key_exists('Payload', $Buffer)) {
-
-                $this->SendDebug('Payload', 'Received Payload with size ' . strlen($Buffer['Payload']), 0);
-            }
-            else{
-               
-                $this->SendDebug('Payload', 'No Payload found', 0);
-                return;
-            }
-
-            if (IPS_GetKernelDate() > 1670886000) {
-                $Buffer['Payload'] = utf8_decode($Buffer['Payload']);
-            }
-
-            $this->SendDebug('Payload', $Buffer['Payload'], 0);
-            $Payload = json_decode($Buffer['Payload'], true);
-
-            if(array_key_exists('success', $Payload)) {
-
-                $this->SendDebug('Payload', 'success indicator found', 0);
-            }
-            else{
-               
-                $this->SendDebug('Payload', 'success indicator NOT found', 0);
-                return;
-            }
-            
-            if ($Payload['success'] == true) {
-
-                $this->SendDebug('ZWAPI', 'OK', 0);
-            }
-            else {
-
-                $this->SendDebug('ZWAPI', 'NOT OK', 0);
-            }
-
-            $devices = $Payload['result'];
-            $this->SendDebug('RESULT', 'Number of devices found: ' . count($devices), 0);
-
-            $deviceDetails = Array();
-
-            foreach ($devices as $currentDevice) {
-
-                if (array_key_exists('name', $currentDevice)) {
-
-                    if ($currentDevice['name']) {
-
-                        $currentDeviceDetails['friendly_name'] = $currentDevice['name'];
-                    }
-                    else {
-
-                        $currentDeviceDetails['friendly_name'] = 'nodeID_' . $currentDevice['id'];
-                    }
-                }
-                else {
-                
-                    $currentDeviceDetails['friendly_name'] = 'nodeID_' . $currentDevice['id'];
-                }
-                $currentDeviceDetails['nodeId'] = $currentDevice['id'];
-                $currentDeviceDetails['type'] = 'Unknown Type';
-                $currentDeviceDetails['vendor'] = $currentDevice['manufacturer'];
-                $currentDeviceDetails['modelID'] = $currentDevice['productLabel'];
-                $currentDeviceDetails['description'] = $currentDevice['productDescription'];
-                $currentDeviceDetails['powerSource'] = 'Unknown';
-                if ($currentDevice['productType'] == 1) {
-
-                    $currentDeviceDetails['powerSource'] = 'Mains';
-                }
-                if ($currentDevice['productType'] == 2) {
-
-                    $currentDeviceDetails['powerSource'] = 'Battery';
-                }
-                
-                array_push($deviceDetails, $currentDeviceDetails);
-            }
-
-            $this->WriteAttributeString('Devices', json_encode($deviceDetails));
-            $this->LogMessage('Fetching devices from zwave-js-ui is complete', KL_MESSAGE);
-        }
     }
 
     public function getDeviceInfo() {
