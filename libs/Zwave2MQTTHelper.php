73a365998ff7739316f0c81c6e94af4a246a9bda
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-07-19 22:51:30 +0200

    sdf

--- libs/Zwave2MQTTHelper.php
+++ libs/Zwave2MQTTHelper.php
@@ -134,7 +134,7 @@ trait Zwave2MQTTHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = $topic . '/set';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/' . $topic . '/set';
         $Data['Payload'] = $payloadJson;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . ' Topic', $Data['Topic'], 0);
